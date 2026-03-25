a042d85a356546450690594f942aa6c05ad2208b
LiseKleinbekman <h.m.kleinbekman@student.rug.nl>
2026-03-25 16:03:32 +0100

    Same code as data_download, just formatted as a function that returns all the values

---
+++ data/data_load_function.py
@@ -0,0 +1,43 @@
+#this uses data from HEPData record 93086, Table 5
+#dataframe uses mass (GeV), counts, bin width and poisson uncertainty
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_data():
+    df = pd.read_csv('data/data_.csv', skipfooter=65, skiprows=11, delimiter=',') #load data into dataframe, skip unnecessary lines
+
+    #make columns
+    df.columns = ['m_center', 'm_lo', 'm_hi', 'counts']
+
+    df['uncertainty'] = np.sqrt(df['counts']) #poisson uncertainty, because no uncertainty specified 
+    df['bin_width'] = df['m_hi'] - df['m_lo']
+
+    #print(df)  #print to check
+
+
+
+    return (
+        df['m_center'].values,
+        df['counts'].values,
+        df['uncertainty'].values,
+        df['bin_width'].values,
+        df['m_lo'].values,
+        df['m_hi'].values
+    ) #return everything into separate columns
+
+# Call the function and unpack the returned values
+m_center, counts, uncertainty, bin_width, m_lo, m_hi = load_data()
+
+#plotting raw data
+#could also plot on linear scale, would remove plt.yscale
+plt.figure(figsize=(10,6))
+plt.errorbar(m_center, counts, yerr=uncertainty, fmt='o', color='black', ms=4, capsize=2, alpha=0.6)
+plt.xlabel(r'$m_{jj}$ [GeV]', fontsize=11)
+plt.ylabel('Counts', fontsize=11)
+plt.yscale('log')  #log scale
+plt.title('Raw Data')
+plt.savefig('data\data_raw.png', dpi=300)
+plt.show()
