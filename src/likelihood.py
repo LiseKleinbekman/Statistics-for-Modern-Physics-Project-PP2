f3d3fec88e5f1ddb01f24bab4e2b6c1f8ef82356
LiseKleinbekman <h.m.kleinbekman@student.rug.nl>
2026-03-25 17:17:55 +0100

    Data load

--- src/likelihood.py
+++ src/likelihood.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from scipy import stats
 from scipy.special import gammaln
 
+
+#Load the data from the data_load_funtion.py file 
 # Add parent directory to path so we can import from data folder
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
